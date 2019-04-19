d2cef1c04ca9645cf3f1b98a45cea174f7b7bad9
alanli-dev <alanli.li@mail.utoronto.ca>
2019-04-19 12:28:32 -0400

    Update siamese_model.py
    [Alan] added initial implementation tuned for omniglot dataset

--- siamese_model.py
+++ siamese_model.py
@@ -5,13 +5,31 @@ class SiameseNetwork(nn.Module) :
     def __init__(self, batch_size):
         super(SiameseNetwork,self).__init__()
 
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 64, 10),
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2)
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 64, 10),  # 64@96*96
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),  # 64@48*48
+            nn.Conv2d(64, 128, 7),
+            nn.ReLU(),    # 128@42*42
+            nn.MaxPool2d(2),   # 128@21*21
+            nn.Conv2d(128, 128, 4),
+            nn.ReLU(), # 128@18*18
+            nn.MaxPool2d(2), # 128@9*9
+            nn.Conv2d(128, 256, 4),
+            nn.ReLU(),   # 256@6*6
         )
-
+        self.features = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
+        self.out = nn.Linear(4096, 1)
+        
+    def forward_one(self, x):
+        x = self.conv(x)
+        x = x.view(x.size()[0], -1)
+        x = self.linear(x)
+        return x
     def forward(self, image1, image2):
-        h1_1 = self.layer1(image1)
+        out1 = self.forward_one(image1)
+        out2 = self.forward_one(image2)
+        dis = torch.abs(out1 - out2)
+        out = self.out(dis)
+        return out
 
-        return h1_1
