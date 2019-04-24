6f9770c91f02d5eae190e20e58385dadb154b499
AbdallaGomaa <abdallahgomaa7@gmail.com>
2019-04-24 19:52:51 -0400

    New siamese training and omniglot data loading

--- siamese_train.py
+++ siamese_train.py
@@ -1,98 +1,111 @@
-from helpers import *
-from omniglot_loader import OmniglotLoader
-import numpy as np
 import torch
-from torch import nn
-from torch import optim
-from siamese_model import SiameseNetwork
+import pickle
+import torchvision
+from torchvision import transforms
+import torchvision.datasets as dset
+from torchvision import transforms
+from omniglot_loader import OmniglotTrain, OmniglotTest
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+from siamese_model import *
+import time
+import numpy as np
+import gflags
+import sys
+from collections import deque
 import os
-
-
-def accuracy(output, target):
-    """Computes the accuracy for multiple binary predictions"""
-    pred = output >= 0.5
-    truth = target >= 0.5
-    acc = pred.eq(truth).float().mean()
-    return np.asscalar(to_data(acc))
+from helpers import *
 
 if __name__ == '__main__':
 
-    omniglot_loader = OmniglotLoader(
-        dataset_path="omniglot", use_augmentation=True, batch_size=32)
+    Flags = gflags.FLAGS
+    gflags.DEFINE_string("train_path", "omniglot/", "training folder")
+    gflags.DEFINE_string("test_path", "omniglot/", 'path of testing folder')
+    gflags.DEFINE_integer("way", 20, "how much way one-shot learning")
+    gflags.DEFINE_string("times", 400, "number of samples to test accuracy")
+    gflags.DEFINE_integer("workers", 4, "number of dataLoader workers")
+    gflags.DEFINE_integer("batch_size", 128, "number of batch size")
+    gflags.DEFINE_float("lr", 0.00006, "learning rate")
+    gflags.DEFINE_integer("show_every", 10, "show result after each show_every iter.")
+    gflags.DEFINE_integer("save_every", 100, "save model after each save_every iter.")
+    gflags.DEFINE_integer("test_every", 100, "test model after each test_every iter.")
+    gflags.DEFINE_integer("max_iter", 50000, "number of iterations before stopping")
+    gflags.DEFINE_string("model_path", "/home/data/pin/model/siamese", "path to store model")
+    gflags.DEFINE_string("gpu_ids", "0,1,2,3", "gpu ids used to train")
 
-    omniglot_loader.split_train_datasets()
+    Flags(sys.argv)
 
-    evaluate_each = 1000
-    train_losses = np.zeros(shape=evaluate_each)
-    train_accuracies = np.zeros(shape=evaluate_each)
-    count = 0
-    earrly_stop = 0
-    # Stop criteria variables
-    best_validation_accuracy = 0.0
-    best_accuracy_iteration = 0
-    validation_accuracy = 0.0
+    data_transforms = transforms.Compose([
+        transforms.RandomAffine(15),
+        transforms.ToTensor()
+    ])
 
-    number_of_iterations = 1000000
-    support_set_size = 20
-    model_name = 'Siamese_Network'
-    model = SiameseNetwork()
-    if torch.cuda.is_available():
-        model.cuda()
+    trainSet = OmniglotTrain(Flags.train_path, transform=data_transforms)
+    testSet = OmniglotTest(Flags.test_path, transform=transforms.ToTensor(), times = Flags.times, way = Flags.way)
+    testLoader = DataLoader(testSet, batch_size=Flags.way, shuffle=False, num_workers=Flags.workers)
 
-    optimizer = optim.Adam(model.parameters(), lr=10e-4, weight_decay=0.0001)
+    trainLoader = DataLoader(trainSet, batch_size=Flags.batch_size, shuffle=False, num_workers=Flags.workers)
+
     loss_fn = torch.nn.BCEWithLogitsLoss(size_average=True)
+    net = SiameseNetwork()
 
-    model.train()
-    for iteration in range(number_of_iterations):
-        images, labels = omniglot_loader.get_train_batch()
+    # multi gpu
+    if len(Flags.gpu_ids.split(",")) > 1:
+        net = torch.nn.DataParallel(net)
 
-        output = model(to_torch_to_var(images[0]), to_torch_to_var(images[1]))
+    if torch.cuda.is_available():
+        net.cuda()
+
+    net.train()
+
+    optimizer = torch.optim.Adam(net.parameters(),lr = Flags.lr )
+    optimizer.zero_grad()
+
+    train_loss = []
+    loss_val = 0
+    time_start = time.time()
+    queue = deque(maxlen=20)
+
+    for batch_id, (img1, img2, label) in enumerate(trainLoader, 1):
+        if batch_id > Flags.max_iter:
+            break
+
+        img1, img2, label = to_var(img1), to_var(img2), to_var(label)
 
         optimizer.zero_grad()
-
-        train_loss = loss_fn(output, labels)
-        train_loss.backward()
+        output = net.forward(img1, img2)
+        loss = loss_fn(output, label)
+        loss_val += loss.item()
+        loss.backward()
         optimizer.step()
+        if batch_id % Flags.show_every == 0 :
+            print('[%d]\tloss:\t%.5f\ttime lapsed:\t%.2f s'%(batch_id, loss_val/Flags.show_every, time.time() - time_start))
+            loss_val = 0
+            time_start = time.time()
+        if batch_id % Flags.save_every == 0:
+            torch.save(net.state_dict(), Flags.model_path + '/model-inter-' + str(batch_id+1) + ".pt")
+        if batch_id % Flags.test_every == 0:
+            right, error = 0, 0
+            for _, (test1, test2) in enumerate(testLoader, 1):
+                test1, test2 = to_var(test1), to_var(test2)
+                output = net.forward(test1, test2).data.cpu().numpy()
+                pred = np.argmax(output)
+                if pred == 0:
+                    right += 1
+                else: error += 1
+            print('*'*70)
+            print('[%d]\tTest set\tcorrect:\t%d\terror:\t%d\tprecision:\t%f'%(batch_id, right, error, right*1.0/(right+error)))
+            print('*'*70)
+            queue.append(right*1.0/(right+error))
+        train_loss.append(loss_val)
+    #  learning_rate = learning_rate * 0.95
 
-        train_accuracy = accuracy(output, labels)
+    with open('train_loss', 'wb') as f:
+        pickle.dump(train_loss, f)
 
-        # validation set
-        if iteration % 100 == 0:
-            print('Iteration %d/%d: Train loss: %f, Train Accuracy: %f' %
-                 (iteration + 1, number_of_iterations, train_loss, train_accuracy))
-
-        # Each 100 iterations perform a one_shot_task and write to tensorboard the
-        # stored losses and accuracies
-        if (iteration + 1) % evaluate_each == 0:
-            number_of_runs_per_alphabet = 40
-            # use a support set size equal to the number of character in the alphabet
-            validation_accuracy = omniglot_loader.one_shot_test(
-                model, support_set_size, number_of_runs_per_alphabet, is_validation=True)
-
-            # Some hyperparameters lead to 100%, although the output is almost the same in
-            # all images.
-            if (validation_accuracy == 1.0 and train_accuracy == 0.5):
-                print('Early Stopping: Gradient Explosion')
-                print('Validation Accuracy = ' +
-                      str(best_validation_accuracy))
-                exit()
-            elif train_accuracy == 0.0:
-                exit()
-            else:
-                # Save the model
-                if validation_accuracy > best_validation_accuracy:
-                    best_validation_accuracy = validation_accuracy
-                    best_accuracy_iteration = iteration
-
-                    if not os.path.exists('./models'):
-                        os.makedirs('./models')
-                    torch.save(model.state_dict(), './models')
-
-        # If accuracy does not improve for 10000 batches stop the training
-        if iteration - best_accuracy_iteration > 10000:
-            print(
-                'Early Stopping: validation accuracy did not increase for 10000 iterations')
-            print('Best Validation Accuracy = ' +
-                  str(best_validation_accuracy))
-            print('Validation Accuracy = ' + str(best_validation_accuracy))
-            break
+    acc = 0.0
+    for d in queue:
+        acc += d
+    print("#"*70)
+    print("final accuracy: ", acc/20)
