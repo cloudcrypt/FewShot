91f425860a88f6d74934d88698a22a39237896ae
AbdallaGomaa <abdallahgomaa7@gmail.com>
2019-04-25 23:45:30 -0400

    Added missing line

--- sm_old_test.py
+++ sm_old_test.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
 
     omniglot_loader = OmniLoader(
         dataset_path="omniglot", use_augmentation=False, batch_size=32)
+    omniglot_loader.split_train_datasets()
 
     model = torch.load(Flags.model_path + "/best_siamese_old_model.pt")
     model.eval()
     if torch.cuda.is_available():
         model.cuda()
-
     evaluation_accuracy = omniglot_loader.one_shot_test(model, 20, 40, False)
 
     print('Final Evaluation Accuracy = ' + str(evaluation_accuracy))
