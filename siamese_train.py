a00e4306e79712732e57371ccb2d69d927758da9
AbdallaGomaa <abdallahgomaa7@gmail.com>
2019-04-25 23:34:50 -0400

    Missing cuda

--- siamese_train.py
+++ siamese_train.py
@@ -101,6 +101,9 @@ if __name__ == '__main__':
 
     model = torch.load(Flags.model_path + "/best_siamese_model.pt")
     model.eval()
+    if torch.cuda.is_available():
+        model.cuda()
+
     evaluation_accuracy = omniglot_loader.one_shot_test(model, 20, 40, False)
 
     print('Final Evaluation Accuracy = ' + str(evaluation_accuracy))
--- sm_old_test.py
+++ sm_old_test.py
@@ -14,6 +14,9 @@ if __name__ == '__main__':
 
     model = torch.load(Flags.model_path + "/best_siamese_old_model.pt")
     model.eval()
+    if torch.cuda.is_available():
+        model.cuda()
+
     evaluation_accuracy = omniglot_loader.one_shot_test(model, 20, 40, False)
 
     print('Final Evaluation Accuracy = ' + str(evaluation_accuracy))
