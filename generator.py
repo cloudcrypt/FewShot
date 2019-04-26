756f09dea82ffa1c864f1c9df0635088a577b268
Daniel D <cloudcrypt@users.noreply.github.com>
2019-04-26 00:37:17 -0400

    Add Generator

--- generator.py
+++ generator.py
@@ -1,36 +1,15 @@
 import torch
 import torch.nn as nn
+from InstanceEncoder import InstanceEncoder
 
-class Generator(nn.Module) :
-    def __init__(self):
+class Generator(nn.Module):
+    def __init__(self, image_size=105):
         super(Generator, self).__init__()
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(1, 64, 10),  # 64@96*96
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  # 64@48*48
-            nn.Conv2d(64, 128, 7),
-            nn.ReLU(),    # 128@42*42
-            nn.MaxPool2d(2),   # 128@21*21
-            nn.Conv2d(128, 128, 4),
-            nn.ReLU(), # 128@18*18
-            nn.MaxPool2d(2), # 128@9*9
-            nn.Conv2d(128, 256, 4),
-            nn.ReLU(),   # 256@6*6
-        )
-        self.features = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
-        self.out = nn.Sequential(nn.Linear(4096, 1), nn.Sigmoid())
-        
-    def forward_one(self, x):
-        x = self.conv(x)
-        x = x.view(x.size()[0], -1)
-        x = self.features(x)
+        self.instance_encoder = InstanceEncoder(image_size=image_size)
+
+    def forward(self, x, z):
+        x = self.instance_encoder(x)
+        x = x.sum(dim=0) / x.shape[0]
+        x = torch.cat((x,z))
         return x
-
-    def forward(self, image1, image2):
-        out1 = self.forward_one(image1)
-        out2 = self.forward_one(image2)
-        dis = torch.abs(out1 - out2)
-        out = self.out(dis)
-        return out
-
