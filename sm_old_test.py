0d741c009e03af6c720711681f997e7b0956892b
AbdallaGomaa <abdallahgomaa7@gmail.com>
2019-04-25 23:32:53 -0400

    Added test

---
+++ sm_old_test.py
@@ -0,0 +1,19 @@
+import torch
+from omni_loader import OmniLoader
+import gflags
+import sys
+
+if __name__ == '__main__':
+    Flags = gflags.FLAGS
+    gflags.DEFINE_string("model_path", "model/siamese", "path to store model")
+
+    Flags(sys.argv)
+
+    omniglot_loader = OmniLoader(
+        dataset_path="omniglot", use_augmentation=False, batch_size=32)
+
+    model = torch.load(Flags.model_path + "/best_siamese_old_model.pt")
+    model.eval()
+    evaluation_accuracy = omniglot_loader.one_shot_test(model, 20, 40, False)
+
+    print('Final Evaluation Accuracy = ' + str(evaluation_accuracy))
